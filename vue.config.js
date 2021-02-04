59ae5e2de0ae9fdde581adcc4a739e3fd1611927
Miguel Mendoza <maestropianistro@gmail.com>
2021-02-04 21:32:46 +0800

    updated PWA settings

--- vue.config.js
+++ vue.config.js
@@ -1,14 +1,17 @@
 module.exports = {
 	transpileDependencies: ["vuetify"],
 	pwa: {
-		name: "Mahog",
-		themeColor: "#E0CC91",
-		msTileColor: "#E0CC91",
-		iconPath: {
-			favicon32: "img/icons/favicon-32x32.png",
-			favicon16: "img/icons/favicon-16x16.png",
-			appleTouchIcon: "img/icons/apple-touch-icon-152x152.png",
-			msTileImage: "img/icons/ms-icon-144x144.png",
-		},
-	},
+      name: 'Mahog',
+      themeColor: '#C0AD74',
+      msTileColor: '#C0AD74',
+      iconPath: {
+        favicon32: 'img/icons/favicon-32x32.png',
+        favicon16: 'img/icons/favicon-16x16.png',
+        appleTouchIcon: 'img/icons/apple-touch-icon-152x152.png',
+        msTileImage: 'img/icons/ms-icon-144x144.png'
+      },
+      manifestOptions: {
+        background_color: '#C0AD74'
+      }
+    },
 };
