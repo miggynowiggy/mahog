e61e8f0de70646c4ef71f16ee8d4dd92f58a0b2d
Miguel Mendoza <mendozajuanmiguel11@gmail.com>
2021-06-10 01:55:52 +0800

    fixed fomatting of the syntax error message

--- src/store/modules/syntax.js
+++ src/store/modules/syntax.js
@@ -51,24 +51,36 @@ export default {
 				let expectedTokens = '';
 				const expectedTokensLength = linesWithExpectedTokens.length;
 				for (let index = 0; index < expectedTokensLength; index++) {
-					// the specific expected token is seen at the second element of the line
 					const line = linesWithExpectedTokens[index];
 					const words = line.split(" ");
 
+					// skip appending the additional expected token if it already exisits on the string holder
+					const numberList = ['nega_float_num_lit', 'float_num_lit', 'nega_num_lit', 'num_lit']
+					const isNumber = numberList.includes(words[1])
+					const skipIteration = expectedTokens.includes("[ number literal ]") && isNumber
+					if (skipIteration) continue;
+
 					// this is just some styling fancy pants on showing the expected tokens
 					if (index + 1 !== expectedTokensLength) {
+						// the specific expected token is seen at the second element of the words array
 						expectedTokens += '[ ' + tokens[words[1]] + ' ] or ';
 					} else {
+						// the specific expected token is seen at the second element of the words array
 						expectedTokens += '[ ' + tokens[words[1]] + ' ]';
 					}
+				}
+
+				// just remove the last "or" string in case that it exists at the end of the expectedToken string
+				if (expectedTokens.lastIndexOf("or") >= 0) {
+					expectedTokens = expectedTokens.substring(0, expectedTokens.length - 4);
 				}
 
 				commit('ADD_ERROR', {
 					type: 'SYN',
 					code: 'syntax-error',
 					message: `
-						Unexpected token -> [ ${currentToken.lexeme} ]
-						instead was expecting -> ${expectedTokens}
+						Unexpected token -> [ ${currentToken.lexeme} ].
+						Instead was expecting -> ${expectedTokens}
 					`,
 					line: currentToken.line,
 					col: currentToken.col
